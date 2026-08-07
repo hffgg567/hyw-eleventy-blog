b7ba72ec280588c24ec668bf77b79811f5cefe83
hffgg567 <132136546+hffgg567@users.noreply.github.com>
2026-08-07 19:56:43 +0800

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: "Eleventy Base Blog v8",
 	url: "https://example.com/",
-	language: "en",
+	language: "zh ",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
 		name: "Your Name Here",
